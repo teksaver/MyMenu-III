a353299afe87fc6079b3e5566f4c18780190e86a
cursan-a <anthony.cursan@contentsquare.com>
2018-02-28 07:10:26 +0900

    Add behaviour in Order class

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -5,13 +5,17 @@ public class Order {
      * Display all available menus in the restaurant.
      */
     public void displayAvailableMenu() {
-
+        System.out.println("Choix menu");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu ?");
     }
     /**
      * Display a selected menu.
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-
+        System.out.println("Vous avez choisi le menu " + nbMenu);
     }
 }
